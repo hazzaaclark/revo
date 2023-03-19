b2e16c374a50343ffc5cf0c15df93b356c1f5072
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-03-19 19:16:37 +0000

    Update os.h

--- include/os.h
+++ include/os.h
@@ -1,6 +1,6 @@
 /* Copyright (C) 2023 Harry Clark */
 
-/* Mario Kart Wii DIscord Rich Presence */
+/* Mario Kart Wii Discord Rich Presence */
 
 /* THIS FILE PERTAINS TO THE FUNCTIONALITY OF THE WII'S OS */
 /* DISCERNING HOW THREADDING WORKS TO PARSE INFORMATION */
