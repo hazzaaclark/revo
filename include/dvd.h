db0b1d409e2264aa58850e0e58b576ebfb351eee
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-03-21 00:40:36 +0000

    Add files via upload

---
+++ include/dvd.h
@@ -0,0 +1,20 @@
+/* Copyright (C) 2023 Harry Clark */
+
+/* Mario Kart Wii Discord Rich Presence */
+
+/* THIS FILE PERTAINS TO THE FUNCTIONALITY OF THE DVD */
+/* STRUCTURE IN RELATION TO THE FUINCTIONALITY OF THE OS */
+
+/* THIS IS TO FURTHER UNDERSTAND THE VARIOUS STATES OF THE */
+/* DVD'S READ AND WRITE PROCESS */
+
+#ifndef DVD
+#define DVD
+
+/* NESTED INCLUDES */
+
+#include "common.h"
+#include "dolphin.h"
+#include "os.h"
+
+#endif
