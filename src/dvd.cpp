eccaf24de394a50db6e981480a1ea154f804ca3a
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-03-22 20:46:30 +0000

    Add files via upload

---
+++ src/dvd.cpp
@@ -0,0 +1,18 @@
+/* Copyright (C) 2023 Harry Clark */
+
+/* Mario Kart Wii Discord Rich Presence */
+
+/* THIS FILE PERTAINS TO AND APPLIES THE FUNCTIONALTY FOR READING */
+/* THE CONTENTS OF THE DISK */
+
+#include "dvd.h"
+
+static inline DVD_ARCHIVE* DVD_CONSTRUCTOR()
+{
+	MULTI_DVD* MULTI_DVD_ARCHIVE = nullptr;
+}
+
+static void DVD_MOUNT(HEAP* ARCHIVE_HEAP)
+{
+	
+}
