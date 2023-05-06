bcef5e92c82393873072891b3ce522d480b77623
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-05-06 15:46:03 +0100

    Update discord.h

--- include/discord.h
+++ include/discord.h
@@ -23,4 +23,17 @@
 extern "C" {}
 #endif
 
+typedef struct DISCORD_RICH_PRESENCE
+{
+	typedef const char* STATE;
+	typedef const char* DETAILS;
+	typedef S64 START;
+	typedef S64 END;
+	typedef const char* LARGE_IMAGE_KEY;
+	typedef const char* LARGE_IMAGE_TEXT;
+	typedef const char* SMALL_IMAGE_KEY;
+	typedef const char* SMALL_IMAGE_TEXT;
+	typedef S8 INSTANCE;
+};
+
 #endif
