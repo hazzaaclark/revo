8c07b30e67faf21d0a2d55412cb7b7e6ef6d7606
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-05-17 21:27:14 +0100

    Add files via upload

---
+++ include/menu.h
@@ -0,0 +1,30 @@
+/* Copyright (C) 2023 Harry Clark */
+
+/* Mario Kart Wii Discord Rich Presence */
+
+/* THIS FILE PERTAINS TO THE FUNCTIONALITY OF THE MENU STATE */
+
+#ifndef MENU
+#define MENU
+
+/* NESTED INCLUDES */
+
+#include "common.h"
+
+typedef enum MENU_STATE : U32
+{
+	IN_RACE              = 0,
+	MAIN_MENU            = 1,
+	MODE_SELECT          = 2,
+	CHARACTER_SELECT     = 3,
+	TRACK_SELECT         = 4,
+	ENGINE_CLASS         = 5,
+	CLEARED              = 6,
+};
+
+typedef struct MENU_MANAGER
+{
+	typedef U16* GET_STATE_COUNT();
+};
+
+#endif
