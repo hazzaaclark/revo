a156efe5c5468fab5c6aa393b06321332b3e09af
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-05-17 19:53:38 +0100

    Add files via upload

---
+++ src/dolphin.cpp
@@ -0,0 +1,19 @@
+/* Copyright (C) 2023 Harry Clark */
+
+/* Mario Kart Wii Discord Rich Presence */
+
+/* THIS FILE PERTAINS TO THE PROCESSES OF THE EMULATION ITSELF AS */
+/* THIS WILL INITILISE METHODS FOR THE GAME RELATIVE TO THE HEADER FILE */
+
+/* NESTED INCLUDES */
+
+#include "dolphin.h"
+
+#ifdef USE_GAME_PARAMS
+
+static inline DOLPHIN_POINTER* GET_DOLPHIN_INSTANCE()
+{
+
+}
+
+#endif
