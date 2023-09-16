7cb9579e167632707a0697f06f32022251f5eaa5
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-09-16 23:09:44 +0100

    Update discord.h

--- include/discord.h
+++ include/discord.h
@@ -12,62 +12,54 @@
 
 #include "common.h"
 #include "dolphin.h"
+#include "menu.h"
 
-#ifndef DISCORD_LIB
-#define DISCORD_EXPORT DECLSPEC 
-#endif
+#if defined(USE_DISCORD_UTIL)
+#define DISCORD_UITL
+#else
+#define DISCORD_UTIL
 
-#ifdef __cplusplus
-extern "C" {}
-#endif
+#define DISCORD_APP_ID "3764283474745677443"
 
-#ifndef DISCORD_REPLY_HANDLES
-#define DISCORD_REPLY_HANDLES
+#define DISCORD_PRESENCE_OP(PRESENCE) \
+inline bool operator==(const DISCORD_PRESENCE &PRESENCE) const { return true; }
 
-#define DISCORD_REPLY_NO 0
-#define DISCORD_REPLY_YES 1
-#define DISCORD_REPLY_IGNORE 2
-#define DISCORD_PARTY_PRIVATE 3
-#define DISCORD_PARTY_PUBLIC 4
-
-#endif
-
-#define DISCORD_APP_ID 993591242724212800
-
-typedef struct DISCORD_RICH_PRESENCE
+typedef struct DISCORD_PRESENCE
 {
-	const char* STATE;
-	const char* DETAILS;
-	S64 START;
-	S64 END;
-	const char* LARGE_IMAGE_KEY;
-	const char* LARGE_IMAGE_TEXT;
-	const char* SMALL_IMAGE_KEY;
-	const char* SMALL_IMAGE_TEXT;
-	S8 INSTANCE;
+	static const char* STATE;
+	static const char* DETAILS;
+	static const char* LARGE_IMAGE_KEY;
+	static const char SMALL_IMAGE_KEY;
+	static const char* LARGE_IMAGE_TEXT;
+	static const char* SMALL_IMAGE_TEXT;
+
+	S64 START_TIME = {};
+	S64 END_TIME = {};
+	S32 PARTY_SIZE = {};
+	S32 PARTY_MAX_SIZE = {};
+
+	typedef void ON_SECTION_CHANGE(MENU_STATE* MENU_STATE);
+
 };
 
-typedef struct DISCORD_USER
+typedef enum CONNECTION
 {
-	const char* USER_ID;
-	const char* USERNAME;
-	const char* DISCRIMNATOR;
-	const char* AVATAR;
+	CONNECTED,
+	BLOCKED,
+	UNSUPPORTED,
 };
 
-typedef struct DISCORD_EVENT_HANDLERS
+DISCORD_PRESENCE* DEFAULT_PRESENCE(DISCORD_PRESENCE* PRESENCE)
 {
-	typedef void(*READY)(const DISCORD_USER* REQUEST);
-	typedef void(*DISCONNECTED)(int ERROR_CODE, const char* MESSAGE);
-	typedef void(*MALFUNCTION)(int ERROR_CODE, const char* MESSAGE);
+	PRESENCE->STATE = "Mario Kart Wii";
+	PRESENCE->DETAILS = "MKW";
+	PRESENCE->START_TIME = 0;
+	PRESENCE->END_TIME = 0;
+	PRESENCE->LARGE_IMAGE_KEY = "LOGO";
+	PRESENCE->LARGE_IMAGE_TEXT = "MKW";
 };
 
 
-DISCORD_EXPORT void DISCORD_INIT_CLIENT(U64 APP_ID, DISCORD_EVENT_HANDLERS* HANDLERS, U32 REGISTER);
-DISCORD_EXPORT void DISCORD_RUN_CALLBACKS();
-DISCORD_EXPORT void DISCORD_UPDATE();
-VOID_FUNCTION(DISCORD_CLEAR);
-VOID_FUNCTION(DISCORD_UPDATE_HANDLERS);
-VOID_FUNCTION(DISCORD_SHUTDOWN);
+#endif
 
 #endif
