6f579521bb0aeaaeaa12db899bf1416358294481
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-09-16 23:39:36 +0100

    updated project type
    updated from a dll injection to a console app that recognises when a mkw iso is loaded
    
    this is just to save complications between os'
    not to mention, a less of a headache to worry about converting the contents of a link library to a proprietary format

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+/* Copyright (C) 2023 Harry Clark */
+
+/* Mario Kart Wii Discord Rich Presence */
+
+/* THIS FILE PERTAINS TO THE MAIN FUNCTIONALITY OF THE PROGRAM */
+
+/* NESTED INCLUDES */
+
+#include "common.h"
+
+/* SYSTEM INCLUDES */
+
+#include <stdio.h>
+#include <Windows.h>
+
+#define API_VER "1"
+
+static void INIT_PROGRAM()
+{
+	printf("Mario Kart Wii - Discord Rich Presence");
+}
+
+int main(int argc, char* argv[])
+{
+	INIT_PROGRAM();
+
+	return 0;
+}
