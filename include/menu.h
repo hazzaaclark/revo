cc9e509b7dde6b55133965c089969acb825fdcc2
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-09-16 23:12:11 +0100

    Update menu.h

--- include/menu.h
+++ include/menu.h
@@ -13,13 +13,17 @@
 
 typedef enum MENU_STATE : U32
 {
-	IN_RACE              = 0,
-	MAIN_MENU            = 1,
-	MODE_SELECT          = 2,
-	CHARACTER_SELECT     = 3,
-	TRACK_SELECT         = 4,
-	ENGINE_CLASS         = 5,
-	CLEARED              = 6,
+	GRAND_PRIX = 0x1E,
+	TIME_TRIALS = 0x1F,
+	VS_1P = 0x20,
+	VS_2P = 0x21,
+	VS_3P = 0x22,
+	VS_4P = 0x23,
+
+	WIIMMFI = 0x68,
+	WIIMMFI_2P = 0x69,
+	WIIMMFI_WAIT = 0x6A,
+	WIIMMFI_WAIT_2P = 0x6B,
 };
 
 #endif
